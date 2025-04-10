1924b3d5d90db7ec523e2f09869cfbf8b0c9f275
Aditi Rajesh <aditir2607@gmail.com>
2025-04-10 17:59:06 +0530

    added task 3 + readme file

---
+++ task03/Program.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+using System.Diagnostics.Metrics;
+
+
+namespace Tasks {
+
+    class Program {
+
+        static void DisplayItems(List<string> tasks) {
+            foreach(string task in tasks) {
+                    Console.WriteLine(task);
+            };
+        }
+
+        static void Main(string[] args) {
+            List<string> tasks = new List<string> {"breakfast","gym","meeting"};
+
+            Console.Write("Enter 1 to add items, 2 to remove and 3 to display: ");
+            bool success = int.TryParse(Console.ReadLine(), out int option);
+            Console.WriteLine();
+
+            if (!success){
+                Console.WriteLine("Please enter an integer");
+            } else if (!new int[] {1,2,3}.Contains(option)) {
+                Console.WriteLine("Please enter either 1,2 or 3");
+            } else {
+                if (option == 1) {
+                    Console.Write("Enter number of items to add to list: ");
+                    bool isnum = int.TryParse(Console.ReadLine().Trim().ToLower(), out int num_items);
+                    if (!isnum) {
+                        Console.WriteLine("Please enter an integer for number of items");
+                    } else {
+                        for (int i=0;i<num_items;i++) {
+                            Console.Write($"Enter task {i+1} to add: ");
+                            tasks.Add(Console.ReadLine().Trim().ToLower());
+                            Console.WriteLine();
+                        }
+                        Console.WriteLine("Updated List: ");
+                        DisplayItems(tasks);
+
+                    };
+                } else if (option==2) {
+                    Console.Write("Enter item to delete: ");
+                    string delete_item = Console.ReadLine().Trim().ToLower();
+                    tasks.Remove(delete_item);
+                    Console.WriteLine();
+                    Console.WriteLine("Successfully deleted item!");
+
+                    Console.WriteLine("Updated List: ");
+                    DisplayItems(tasks);
+
+                } else {
+                    foreach(string task in tasks) {
+                        Console.WriteLine(task);
+                    };
+                    Console.WriteLine("Sucessfully printed all the tasks!");
+                };
+            };
+
+        }
+    }
+}
---
+++ task03/README.md
@@ -0,0 +1,18 @@
+# TASK 3: 3. **Basic Collections and String Manipulation
+
+## Requirements:
+    - Write a program to manage a list of strings (e.g., names or tasks).
+    - Use a `List<string>` to store items.
+    - Allow the user to add, remove, and display items.
+    - Utilize loops and basic string methods (like `Trim()`, `ToUpper()`) to process user input.
+
+## Outputs:
+
+### Display items:
+![](display_items.png)
+
+### Add items:
+![](add_item.png)
+
+### Delete items:
+![](delete_items.png)
