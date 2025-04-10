64990ffc08418a09e88ec3ba1c2e162d076799ac
Aditi Rajesh <aditir2607@gmail.com>
2025-04-10 17:52:05 +0530

    added task01 + readme file

---
+++ task01/Program.cs
@@ -0,0 +1,29 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System;
+
+namespace HelloWorld {
+    class Program {
+        static void Main(string[] args) {
+            Console.Write("Enter integer to find factorial for: ");
+            int number = Convert.ToInt32(Console.ReadLine());
+
+            if (number < 0) {
+                Console.WriteLine("The number must be a positive integer. Try again");
+            } else{
+                int result = FindFactorial(number,1);
+                Console.WriteLine("The factorial is "+result);
+            };
+        }
+
+        static int FindFactorial(int number,int result) {
+            result *= number;
+            number -= 1;
+            if (number>0) {
+                return FindFactorial(number,result);
+            } else {
+                return result;
+            };
+
+        }
+    }
+}
---
+++ task01/README.md
@@ -0,0 +1,15 @@
+# TASK 1: 1. **Basic Data Types, Control Structures, and Methods
+
+## Requirements:
+    - Write a console application that calculates the factorial of a given number.
+    - Read an integer from the user.
+    - Validate the input (ensure it’s a positive integer).
+    - Use loops (or recursion) to calculate the factorial.
+    - Display the result in the console.
+
+## Output:
+### Invalid input:
+![](invalid_input.png)
+
+### Valid input:
+![](correctnum_op.png)
